2f856a5abfa9a9a880925aa34152fdc466cfe382
kimYejin <kimphoby0125@gmail.com>
2026-01-05 20:53:57 +0900

    initial setting

---
+++ src/test/java/personal/yejin/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,14 @@
+package personal.yejin.delivery;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
