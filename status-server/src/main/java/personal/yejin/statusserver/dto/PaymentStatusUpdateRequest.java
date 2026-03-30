ab492a2022f0523139bf507a5ea897f890e9077b
kimYejin <kimphoby0125@gmail.com>
2026-03-30 15:44:28 +0900

    [feat] correlationId, PaymentStatus 유효성 검증 추가

--- status-server/src/main/java/personal/yejin/statusserver/dto/PaymentStatusUpdateRequest.java
+++ status-server/src/main/java/personal/yejin/statusserver/dto/PaymentStatusUpdateRequest.java
@@ -1,10 +1,12 @@
 package personal.yejin.statusserver.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import personal.yejin.model.PaymentStatus;
 
 public record PaymentStatusUpdateRequest(
-        String correlationId,
-        PaymentStatus status,
+        @NotBlank String correlationId,
+        @NotNull PaymentStatus status,
         String failureReason
 ) {
 }
