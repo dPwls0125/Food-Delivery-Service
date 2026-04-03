0d80b26627b3660cf4e21e9d36d38c8acbfcab81
kimYejin <kimphoby0125@gmail.com>
2026-04-03 17:51:29 +0900

    [feat] virtualThread config에 EnableAsync annotation 추가

--- Payment/src/main/java/personal/yejin/config/ExecutorConfig.java
+++ Payment/src/main/java/personal/yejin/config/ExecutorConfig.java
@@ -2,10 +2,12 @@ package personal.yejin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+@EnableAsync
 @Configuration
 public class ExecutorConfig {
     @Bean
